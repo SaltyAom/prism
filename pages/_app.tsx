7fc7c1abae92ede16f327c92bc8217b12499d451
SaltyAom <aomkirby@mystiar.com>
2020-02-17 00:07:27 +0700

    :wrench: Add viewport-fit=cover, fix position

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,28 +1,40 @@
-import App from 'next/app'
+import { Fragment } from 'react'
 
-import "styles/init.styl"
+import App from 'next/app'
+import Head from 'next/head'
+
+import 'styles/init.styl'
 
 class Next extends App {
-	componentDidMount() {
-		document.addEventListener("touchstart", () => null, false)
+    componentDidMount() {
+        document.addEventListener('touchstart', () => null, false)
 
-		if (
-			"serviceWorker" in navigator &&
-			process.env.NODE_ENV === "production" &&
-			typeof window !== "undefined"
-		)
-			window.addEventListener("load", () => {
-				navigator.serviceWorker.register(
-					"service-worker.js",
-					{ scope: "/" }
-				)
-			})
-	}
+        if (
+            'serviceWorker' in navigator &&
+            process.env.NODE_ENV === 'production' &&
+            typeof window !== 'undefined'
+        )
+            window.addEventListener('load', () => {
+                navigator.serviceWorker.register('service-worker.js', {
+                    scope: '/',
+                })
+            })
+    }
 
     render() {
         let { Component, pageProps } = this.props
 
-        return <Component {...pageProps} />
+        return (
+            <Fragment>
+                <Head>
+                    <meta
+                        name="viewport"
+                        content="width=device-width, initial-scale=1.0, viewport-fit=cover"
+                    />
+                </Head>
+                <Component {...pageProps} />
+            </Fragment>
+        )
     }
 }
 
